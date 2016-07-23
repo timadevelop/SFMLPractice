b94c77eff5c333e525c10c380eeeadde8891bef5
timadevelop <timadevelop@gmail.com>
2016-07-23 21:24:09 +0300

    Best continuous integration ever (2)

---
+++ build/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vtimofeev/projects/SFMLPractice/Enemy.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/Enemy.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/Organism.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/Organism.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/Player.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/Player.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/Wall.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/Wall.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/entity.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/entity.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/helpers/randoms.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/helpers/randoms.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/main.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/main.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/projectile.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/projectile.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/scenes/GameScene.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/scenes/GameScene.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/scenes/MenuScene.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/scenes/MenuScene.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/scenes/Scene.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/scenes/Scene.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/scenes/Scenes.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/scenes/Scenes.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/scenes/TheEndScene.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/scenes/TheEndScene.cpp.o"
+  "/home/vtimofeev/projects/SFMLPractice/textDisplay.cpp" "/home/vtimofeev/projects/SFMLPractice/build/CMakeFiles/Game.dir/textDisplay.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/main.cpp.o"
+  "CMakeFiles/Game.dir/entity.cpp.o"
+  "CMakeFiles/Game.dir/Player.cpp.o"
+  "CMakeFiles/Game.dir/projectile.cpp.o"
+  "CMakeFiles/Game.dir/Enemy.cpp.o"
+  "CMakeFiles/Game.dir/helpers/randoms.cpp.o"
+  "CMakeFiles/Game.dir/textDisplay.cpp.o"
+  "CMakeFiles/Game.dir/Wall.cpp.o"
+  "CMakeFiles/Game.dir/Organism.cpp.o"
+  "CMakeFiles/Game.dir/scenes/Scene.cpp.o"
+  "CMakeFiles/Game.dir/scenes/Scenes.cpp.o"
+  "CMakeFiles/Game.dir/scenes/GameScene.cpp.o"
+  "CMakeFiles/Game.dir/scenes/MenuScene.cpp.o"
+  "CMakeFiles/Game.dir/scenes/TheEndScene.cpp.o"
+  "Game.pdb"
+  "Game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
